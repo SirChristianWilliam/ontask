562642140575b7d731e735030ac0efafac2f14ce
Chris Maki <c.william.developer@gmail.com>
2023-04-27 18:14:00 -0500

    Ensure routing between two different components (home and signin components) display correctly

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+import GlassPane from "@/components/GlassPane";
+
+export default function AuthRootLayout({ children }) {
+
+    return (
+        <html lang="en">
+            <head />
+            <body className="h-screen w-screen rainbow-mesh p-6">
+                <GlassPane className="w-full h-full flex item-center justify-center">
+                    {children}
+                </GlassPane>
+
+            </body>
+        </html>
+    )
+}
