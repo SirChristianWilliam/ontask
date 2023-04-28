152bbc8291361dc1c9933eb98b9bb1e7e7d3a599
Chris Maki <c.william.developer@gmail.com>
2023-04-27 19:52:08 -0500

    Add Card.tsx component

---
+++ components/Card.tsx
@@ -0,0 +1,16 @@
+import clsx from "clsx";
+// Alternative to clsx is using back tics or plus sign
+const Card = ({ className, children }) => {
+  return (
+    <div
+      className={clsx(
+        "rounded-3xl px-10 py-4 drop-shadow-xl bg-white",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Card;
