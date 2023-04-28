c54f06e1e08960a953c7edb5db52337dec750275
Chris Maki <c.william.developer@gmail.com>
2023-04-28 16:24:50 -0500

    Set up api.ts component to make API requests to API routes for user sign in and registration

---
+++ lib/api.ts
@@ -0,0 +1,33 @@
+export const fetcher = async ({url, method, body, json = true}) => {
+   // Scott Moss recommends to scrap Axios, use fetch when possible
+    const res = await fetch(url, {
+        method, 
+        ...body(body && {body: JSON.stringify(body)}), 
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+
+    if (!res.ok) {
+        throw new Error('API error')
+    }
+
+    if (json) {
+        const data = await res.json()
+        return data.data
+    }
+}
+
+export const register = (user) => {
+    return fetcher({url: '/api/register', method: 'post', body: user})
+}
+
+export const signin = (user) => {
+    return fetcher({url: '/api/signin', method: 'post', body: user})
+}
+
+ 
+
+
+
