3987aa25edb51687f60cdf524b511628b0aadd8c
Chris Maki <c.william.developer@gmail.com>
2023-04-29 17:27:55 -0500

    Add JSON web token authentication and validation

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,4 +5,24 @@ import { db } from "./db";
 export const hashPassword = (password) => bcrypt.hash(password, 10);
 
 export const comparePasswords = (plainTextPassword, hashedPassword) =>
-  bcrypt.compare(plainTextPassword, hashedPassword);
+    bcrypt.compare(plainTextPassword, hashedPassword);
+
+export const createJWT = user => {
+    const iat = Math.floor(Date.now() / 1000);
+    const exp = iat + 60 * 60 * 24 * 7;
+
+    return new SignJWT({ payload: { id: user.id, email: user.email } })
+        .setProtectedHeader({ alg: "HS256", typ: "JWT" })
+        .setExpirationTime(exp)
+        .setIssuedAt(iat)
+        .setNotBefore(iat)
+        .sign(new TextEncoder().encode(process.env.JWT_SECRET));
+}
+
+export const validateJWT = async (jwt) => {
+    const { payload } = await jwtVerify(
+        jwt,
+        new TextEncoder().encode(process.env.JWT_SECRET)
+    );
+    return payload.payload as any;
+}
