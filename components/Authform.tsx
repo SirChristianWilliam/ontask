3125e27f6e6d17d1ed524029117b82e1bd5ab3c6
Chris Maki <c.william.developer@gmail.com>
2023-04-30 20:52:58 -0500

    Fix <Card> lack of className error

--- components/Authform.tsx
+++ components/Authform.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
 import Button from "./Button";
-import Card from "./Card";
 import Input from "./Input";
+import Card from "./Card";
 
 const registerContent = {
     linkUrl: "/signin",
@@ -51,89 +51,89 @@ const AuthForm = ({mode}) => {
     const content = mode === 'register' ? registerContent : signinContent
   
     return (
-      <Card>
-        <div className="w-full">
-          <div className="text-center">
-            <h2 className="text-3xl mb-2">{content.header}</h2>
-            <p className="tex-lg text-black/25">{content.subheader}</p>
-          </div>
-          <form onSubmit={handleSubmit} className="py-10 w-full">
-            {mode === "register" && (
-              <div className="flex mb-8 justify-between">
-                <div className="pr-2">
-                  <div className="text-lg mb-4 ml-2 text-black/50">
-                    First Name
-                  </div>
-                  <Input
-                    required
-                    placeholder="First Name"
-                    value={formState.firstName}
-                    className="border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full"
-                    onChange={(e) =>
-                      setFormState((s) => ({ ...s, firstName: e.target.value }))
-                    }
-                  />
-                </div>
-                <div className="pl-2">
-                  <div className="text-lg mb-4 ml-2 text-black/50">Last Name</div>
-                  <Input
-                    required
-                    placeholder="Last Name"
-                    value={formState.lastName}
-                    className="border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full"
-                    onChange={(e) =>
-                      setFormState((s) => ({ ...s, lastName: e.target.value }))
-                    }
-                  />
-                </div>
-              </div>
-            )}
-            <div className="mb-8">
-              <div className="text-lg mb-4 ml-2 text-black/50">Email</div>
-              <Input
-                required
-                type="email"
-                placeholder="Email"
-                value={formState.email}
-                className="border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full"
-                onChange={(e) =>
-                  setFormState((s) => ({ ...s, email: e.target.value }))
-                }
-              />
-            </div>
-            <div className="mb-8">
-              <div className="text-lg mb-4 ml-2 text-black/50">Password</div>
-              <Input
-                required
-                value={formState.password}
-                type="password"
-                placeholder="Password"
-                className="border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full"
-                onChange={(e) =>
-                  setFormState((s) => ({ ...s, password: e.target.value }))
-                }
-              />
-            </div>
-            <div className="flex items-center justify-between">
-              <div>
-                <span>
-                  <Link
-                    href={content.linkUrl}
-                    className="text-blue-600 font-bold"
-                  >
-                    {content.linkText}
-                  </Link>
-                </span>
-              </div>
-              <div>
-                <Button type="submit" intent="secondary">
-                  {content.buttonText}
-                </Button>
-              </div>
-            </div>
-          </form>
+     <Card className="">
+      <div className="w-full">
+        <div className="text-center">
+          <h2 className="text-3xl mb-2">{content.header}</h2>
+          <p className="tex-lg text-black/25">{content.subheader}</p>
         </div>
-      </Card>
+        <form onSubmit={handleSubmit} className="py-10 w-full">
+          {mode === "register" && (
+            <div className="flex mb-8 justify-between">
+              <div className="pr-2">
+                <div className="text-lg mb-4 ml-2 text-black/50">
+                  First Name
+                </div>
+                <Input
+                  required
+                  placeholder="First Name"
+                  value={formState.firstName}
+                  className="border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full"
+                  onChange={(e) =>
+                    setFormState((s) => ({ ...s, firstName: e.target.value }))
+                  }
+                />
+              </div>
+              <div className="pl-2">
+                <div className="text-lg mb-4 ml-2 text-black/50">Last Name</div>
+                <Input
+                  required
+                  placeholder="Last Name"
+                  value={formState.lastName}
+                  className="border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full"
+                  onChange={(e) =>
+                    setFormState((s) => ({ ...s, lastName: e.target.value }))
+                  }
+                />
+              </div>
+            </div>
+          )}
+          <div className="mb-8">
+            <div className="text-lg mb-4 ml-2 text-black/50">Email</div>
+            <Input
+              required
+              type="email"
+              placeholder="Email"
+              value={formState.email}
+              className="border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full"
+              onChange={(e) =>
+                setFormState((s) => ({ ...s, email: e.target.value }))
+              }
+            />
+          </div>
+          <div className="mb-8">
+            <div className="text-lg mb-4 ml-2 text-black/50">Password</div>
+            <Input
+              required
+              value={formState.password}
+              type="password"
+              placeholder="Password"
+              className="border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full"
+              onChange={(e) =>
+                setFormState((s) => ({ ...s, password: e.target.value }))
+              }
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <div>
+              <span>
+                <Link
+                  href={content.linkUrl}
+                  className="text-blue-600 font-bold"
+                >
+                  {content.linkText}
+                </Link>
+              </span>
+            </div>
+            <div>
+              <Button type="submit" intent="secondary">
+                {content.buttonText}
+              </Button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </Card>
     )
   }
   
